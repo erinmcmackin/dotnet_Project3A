808014ef61baf41fabaec67cbd424804c554df5a
Erin McMackin <mcmackinerin@gmail.com>
2018-10-29 18:22:56 -0400

    FormatException created and tested

--- erin_mcmackin_Project3A/CustomCalculator/frmTemp.cs
+++ erin_mcmackin_Project3A/CustomCalculator/frmTemp.cs
@@ -29,7 +29,12 @@ namespace CustomCalculator
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+            // ==========
+            // ORIGINAL CODE WITH IF-ELSE
+            // ==========
+
             // Found validation method on page 203 (Chapter 7) of Murach's C#2015
+            /*
             decimal checkTemp;
             if (!(Decimal.TryParse(txtOrigTemp.Text, out checkTemp)))
             {
@@ -91,6 +96,73 @@ namespace CustomCalculator
                 txtConvTemp.Text = calcTemp.ToString();
                 // Move cursor back to Original Temp fied
                 txtOrigTemp.Focus();
+            }
+            */
+
+            // ==========
+            // USING TRY PARSE METHOD
+            // ==========
+
+
+            try
+            {
+                decimal checkTemp;
+                Decimal.TryParse(txtOrigTemp.Text, out checkTemp);
+
+                decimal origTemp = Convert.ToDecimal(txtOrigTemp.Text);
+                decimal calcTemp = 0;
+
+                if (radioCel1.Checked == true)
+                {
+                    if (radioCel2.Checked == true)
+                    {
+                        calcTemp = origTemp;
+                    }
+                    else if (radioFahr2.Checked == true)
+                    {
+                        calcTemp = Math.Round((origTemp * 1.8m) + 32);
+                    }
+                    else if (radioKel2.Checked == true)
+                    {
+                        calcTemp = Math.Round(origTemp + 273m);
+                    }
+                }
+                else if (radioFahr1.Checked == true)
+                {
+                    if (radioCel2.Checked == true)
+                    {
+                        calcTemp = Math.Round((origTemp - 32) * .5556m);
+                    }
+                    else if (radioFahr2.Checked == true)
+                    {
+                        calcTemp = origTemp;
+                    }
+                    else if (radioKel2.Checked == true)
+                    {
+                        calcTemp = Math.Round((origTemp + 459.67m) * .5556m);
+                    }
+                }
+                else if (radioKel1.Checked == true)
+                {
+                    if (radioCel2.Checked == true)
+                    {
+                        calcTemp = Math.Round(origTemp - 273m);
+                    }
+                    else if (radioFahr2.Checked == true)
+                    {
+                        calcTemp = Math.Round((origTemp * 1.8m) - 459.67m);
+                    }
+                    else if (radioKel2.Checked == true)
+                    {
+                        calcTemp = origTemp;
+                    }
+                }
+
+                // Insert calculated new teperature into Converted Temp field
+                txtConvTemp.Text = calcTemp.ToString();
+                // Move cursor back to Original Temp fied
+                txtOrigTemp.Focus();
+
             }
 
 
